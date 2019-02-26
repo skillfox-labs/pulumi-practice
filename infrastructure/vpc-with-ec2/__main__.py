0b6cdd3d2b7b5520dc707ad35f6589c42242af4f
Tim Condit <tcondit@gmail.com>
2019-02-26 13:23:39 -0800

    Add initial networking

--- infrastructure/vpc-with-ec2/__main__.py
+++ infrastructure/vpc-with-ec2/__main__.py
@@ -1,11 +1,33 @@
 import pulumi
 from pulumi_aws import ec2, s3
 
-vpc = ec2.Vpc(resource_name = 'new-vpc', cidr_block = "10.0.0.0/16")
-subnet = ec2.Subnet(resource_name = 'new-subnet', vpc_id = vpc.id, cidr_block = "10.0.0.0/20")
-sg = ec2.SecurityGroup(resource_name = 'new-sg', vpc_id = vpc.id)
-ig = ec2.InternetGateway(resource_name = 'new-ig', vpc_id = vpc.id)
-bucket = s3.Bucket('my-bucket')
+_az = 'us-west-2b'  # t2.micro not supported in us-west-2
+
+vpc = ec2.Vpc(resource_name = 'new-vpc',
+        cidr_block = '10.0.0.0/16',
+        tags = {'Name': 'infra vpc', 'Creator': 'timc'})
+
+subnet = ec2.Subnet(resource_name = 'new-subnet',
+        vpc_id = vpc.id,
+        cidr_block = '10.0.0.0/20',
+        availability_zone = _az,
+        tags = {'Name': 'infra subnet', 'Creator': 'timc'})
+
+ig = ec2.InternetGateway(resource_name = 'new-ig',
+        vpc_id = vpc.id,
+        tags = {'Name': 'infra internet gateway', 'Creator': 'timc'})
+
+sg = ec2.SecurityGroup(resource_name = 'new-sg',
+        description = 'HTTP and SSH ingress',
+        vpc_id = vpc.id,
+        ingress = [
+            {'protocol': 'tcp', 'fromPort': 22, 'toPort': 22, 'cidrBlocks': ['0.0.0.0/0']},
+            {'protocol': 'tcp', 'fromPort': 80, 'toPort': 80, 'cidrBlocks': ['0.0.0.0/0']},
+            ],
+        tags = {'Name': 'infra security group', 'Creator': 'timc'})
+
+bucket = s3.Bucket(resource_name = 'new-bucket',
+        tags = {'Name': 'infra bucket', 'Creator': 'timc'})
 
 # TODO add instance to new VPC
 #   seems like that's by association w/ subnet
@@ -17,18 +39,26 @@ bucket = s3.Bucket('my-bucket')
 #   edit: unsupported feature on the AWS side
 #   edit: maybe a Pulumi usability bug? This will cause occasional failures if not explicit
 
+# TODO tags
 server = ec2.Instance(
-        resource_name = 'webserver-www',
+        resource_name = 'new-ec2',
         ami = 'ami-032509850cf9ee54e',  # TypeError if not present
         instance_type = 't2.micro',     # TypeError if not present
-        associate_public_ip_address = False,
         security_groups = [sg.id],
+        availability_zone = _az, 
         subnet_id = subnet.id,
+        associate_public_ip_address = False,
+
+        # TODO `Quiver`: `Pulumi > Questions > Adding tags forces EC2 replacement?`
+        #   edit: I also changed the instance's `resource_name`
+        tags = {'Name': 'infra ec2', 'Creator': 'timc'}
         )
 
 # TODO bug? If you include `associate_with_private_ip=server.private_ip` but
 # leave off `instance=server.id`, the association is not created
-eip = ec2.Eip(resource_name='new-eip', instance=server.id, associate_with_private_ip=server.private_ip)
+eip = ec2.Eip(resource_name='new-eip',
+        instance=server.id,
+        associate_with_private_ip=server.private_ip)
 
 # Export the DNS name of the bucket
 pulumi.export('vpcID', vpc.id)
