7d9b6fa9e876085660c3350ab4939683c8a01792
Tim Condit <tcondit@gmail.com>
2019-02-26 19:06:31 -0800

    Fixed route table issue
    * documented in the file
    * still need to add a route table association
    * also 's/ = /=/g' all over the place, but may undo it
    * cleanup coming soon

--- infrastructure/vpc-with-ec2/__main__.py
+++ infrastructure/vpc-with-ec2/__main__.py
@@ -1,21 +1,21 @@
 import pulumi
 from pulumi_aws import ec2, s3
 
-_az = 'us-west-2b'  # t2.micro not supported in us-west-2
+_az='us-west-2b'  # t2.micro not supported in us-west-2
 
-vpc = ec2.Vpc(resource_name = 'new-vpc',
-        cidr_block = '10.0.0.0/16',
-        tags = {'Name': 'infra vpc', 'Creator': 'timc'})
+vpc=ec2.Vpc(resource_name='new-vpc',
+        cidr_block='10.0.0.0/16',
+        tags={'Name': 'infra vpc', 'Creator': 'timc'})
 
-subnet = ec2.Subnet(resource_name = 'new-subnet',
-        vpc_id = vpc.id,
-        cidr_block = '10.0.0.0/20',
-        availability_zone = _az,
-        tags = {'Name': 'infra subnet', 'Creator': 'timc'})
+subnet=ec2.Subnet(resource_name='new-subnet',
+        vpc_id=vpc.id,
+        cidr_block='10.0.0.0/20',
+        availability_zone=_az,
+        tags={'Name': 'infra subnet', 'Creator': 'timc'})
 
-igw = ec2.InternetGateway(resource_name = 'new-igw',
-        vpc_id = vpc.id,
-        tags = {'Name': 'infra internet gateway', 'Creator': 'timc'})
+igw=ec2.InternetGateway(resource_name='new-igw',
+        vpc_id=vpc.id,
+        tags={'Name': 'infra internet gateway', 'Creator': 'timc'})
 
 # Working thru this issue on the Pulumi Slack workspace. It's not documented
 # correctly yet. Here's the response I got on 2/26:
@@ -28,33 +28,53 @@ igw = ec2.InternetGateway(resource_name = 'new-igw',
 #
 # TODO Is there a simpler way to pass the IGW to the route table, or is `rt` needed
 # here?
+
+# FIXME 1 before adding the missing parameter
 #
-rt = ec2.RouteTable('new-rt',
-        vpc_id = vpc.id,
-        routes = [{'gateway_id': igw.id}],
-        tags = {'Name': 'infra route table', 'Creator': 'timc'})
+# rt=ec2.RouteTable('new-rt', vpc_id=vpc.id, routes=[{'gateway_id': igw.id}])
+#
+# * creating urn:pulumi:dev::vpc-with-ec2::aws:ec2/routeTable:RouteTable::new-rt: MissingParameter: The request must contain the parameter destinationCidrBlock or destinationIpv6CidrBlock
 
-route = ec2.Route('default-route',
-        destination_cidr_block = '0.0.0.0/0',
-        gateway_id = igw.id,
-        route_table_id = rt.id)
+# FIXME 2 after adding the missing parameter (with snake_case)
+#
+# rt=ec2.RouteTable('new-rt', vpc_id=vpc.id, routes=[{'gateway_id': igw.id}], destination_cidr_block='0.0.0.0/0')
+#
+# TypeError: __init__() got an unexpected keyword argument 'destination_cidr_block'
 
-sg = ec2.SecurityGroup(resource_name = 'new-sg',
-        description = 'HTTP and SSH ingress',
-        vpc_id = vpc.id,
-        ingress = [
+# FIXME 3 after getting a reply on Slack
+#
+# rt=ec2.RouteTable('new-rt', vpc_id=vpc.id, routes=[{'gateway_id': igw.id, 'destination_cidr_block': '0.0.0.0/0'}])
+#
+# error: aws:ec2/routeTable:RouteTable resource 'new-rt' has a problem: route.0: invalid or unknown key: destination_cidr_block
+
+# FIXED! s/destination_cidr_block/cidr_block/g
+
+rt=ec2.RouteTable('new-rt',
+        vpc_id=vpc.id,
+        routes=[{'gateway_id': igw.id, 'cidr_block': '0.0.0.0/0'}],
+        tags={'Name': 'infra route table', 'Creator': 'timc'})
+
+route=ec2.Route('default-route',
+        destination_cidr_block='0.0.0.0/0',
+        gateway_id=igw.id,
+        route_table_id=rt.id)
+
+sg=ec2.SecurityGroup(resource_name='new-sg',
+        description='HTTP and SSH ingress',
+        vpc_id=vpc.id,
+        ingress=[
             {'protocol': 'tcp', 'fromPort': 22, 'toPort': 22, 'cidrBlocks': ['0.0.0.0/0']},
             {'protocol': 'tcp', 'fromPort': 80, 'toPort': 80, 'cidrBlocks': ['0.0.0.0/0']},
             ],
-        tags = {'Name': 'infra security group', 'Creator': 'timc'})
+        tags={'Name': 'infra security group', 'Creator': 'timc'})
 
-bucket = s3.Bucket(resource_name = 'new-bucket',
-        tags = {'Name': 'infra bucket', 'Creator': 'timc'})
+bucket=s3.Bucket(resource_name='new-bucket',
+        tags={'Name': 'infra bucket', 'Creator': 'timc'})
 
 # TODO add instance to new VPC
 #   seems like that's by association w/ subnet
 #
-# TODO bug? `associate_public_ip_address = False` doesn't seem to do anything
+# TODO bug? `associate_public_ip_address=False` doesn't seem to do anything
 #   edit: after `pulumi destroy` and `pulumi update`, seems to be working
 #
 # TODO bug? See ~/z/src/github.com/tcondit/idea-foundry/bug-and-doc-fixes/01-pulumi-update-ec2-az.md
@@ -62,25 +82,27 @@ bucket = s3.Bucket(resource_name = 'new-bucket',
 #   edit: maybe a Pulumi usability bug? This will cause occasional failures if not explicit
 
 # TODO tags
-server = ec2.Instance(
-        resource_name = 'new-ec2',
-        ami = 'ami-032509850cf9ee54e',  # TypeError if not present
-        instance_type = 't2.micro',     # TypeError if not present
-        security_groups = [sg.id],
-        availability_zone = _az,
-        subnet_id = subnet.id,
-        associate_public_ip_address = False,
+server=ec2.Instance(
+        resource_name='new-ec2',
+        ami='ami-032509850cf9ee54e',  # TypeError if not present
+        instance_type='t2.micro',     # TypeError if not present
+        security_groups=[sg.id],
+        availability_zone=_az,
+        subnet_id=subnet.id,
+        associate_public_ip_address=False,
 
         # TODO `Quiver`: `Pulumi > Questions > Adding tags forces EC2 replacement?`
         #   edit: I also changed the instance's `resource_name`
-        tags = {'Name': 'infra ec2', 'Creator': 'timc'}
+        tags={'Name': 'infra ec2', 'Creator': 'timc'}
         )
 
 # TODO bug? If you include `associate_with_private_ip=server.private_ip` but
 # leave off `instance=server.id`, the association is not created
-eip = ec2.Eip(resource_name='new-eip',
+eip=ec2.Eip(resource_name='new-eip',
         instance=server.id,
-        associate_with_private_ip=server.private_ip)
+        associate_with_private_ip=server.private_ip,
+        tags={'Name': 'infra eip', 'Creator': 'timc'}
+        )
 
 # Export the DNS name of the bucket
 pulumi.export('vpcID', vpc.id)
